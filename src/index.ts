4fee81b9ae654827ec793776e9c97918770e4d54
nathanjpak <nathan.j.pak@gmail.com>
2023-02-14 14:47:26 -0800

    Set up boilerplate graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('Hello world');
+import 'reflect-metadata';
+
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import express from 'express';
+import {buildTypeDefsAndResolvers, Resolver, Query} from 'type-graphql';
+import cors from 'cors';
+import {json} from 'body-parser';
+
+// test resolver
+@Resolver()
+class testResolver {
+  @Query(() => String)
+  async tests() {
+    return 'Hello World';
+  }
+}
+
+(async () => {
+  const app = express();
+
+  const {typeDefs, resolvers} = await buildTypeDefsAndResolvers({
+    resolvers: [testResolver],
+  });
+
+  const server = new ApolloServer({typeDefs, resolvers});
+
+  await server.start();
+
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(server)
+  );
+
+  app.listen(8099, () => {
+    console.log('Server started at port 8099');
+  });
+})();
