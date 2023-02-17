c26e828dd2970fa37f5ac5c53b11422b72aefa8c
nathanjpak <nathan.j.pak@gmail.com>
2023-02-16 22:35:00 -0800

    Add some queries

--- src/modules/template/Resolver.ts
+++ src/modules/template/Resolver.ts
@@ -1,4 +1,4 @@
-import {Resolver, Mutation, Arg, Ctx} from 'type-graphql';
+import {Resolver, Mutation, Arg, Ctx, Query} from 'type-graphql';
 
 import {Template} from '../../entity/Template';
 import {MyContext} from '../../types/MyContext';
@@ -20,4 +20,20 @@ export class TemplateResolver {
 
     return template;
   }
+
+  @Query(() => Template, {nullable: true})
+  async findTemplate(@Arg('id') id: number): Promise<Template | null> {
+    const template = await Template.findOneBy({id: id});
+
+    return template;
+  }
+
+  // @Query(() => Template, {nullable: true})
+  // async findTemplatesByUser(@Ctx() ctx: MyContext): Promise<Template[] | null> {
+  //   if (!ctx.req.session.userId) return null;
+
+  //   const templates = Template.findBy({ownerId: ctx.req.session.userId});
+
+  //   return templates;
+  // }
 }
